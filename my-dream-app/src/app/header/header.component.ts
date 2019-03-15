d747f4dd250fc5113dcab2c7abf69cf41d98ff6e
Yusong Du <yd3127@mun.ca>
2019-03-15 00:42:58 -0230

    new component

---
+++ my-dream-app/src/app/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: "app-header", // the header html tag for the template
+    templateUrl: './header.component.html'
+})
+export class HeaderComponent{
+
+}
