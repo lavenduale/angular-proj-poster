031c92c22d3c3190454ee0408a263f4641582b8c
Yusong Du <yd3127@mun.ca>
2019-05-22 01:58:27 -0230

    delete old version

--- my-dream-app/src/app/header/header.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: "app-header", // the header html tag for the template
-    templateUrl: './header.component.html'
-})
-export class HeaderComponent{
-
-}
