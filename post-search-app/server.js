8c40cfa0ba4aa622a3e5eaf5a99491c3ec710b52
Yusong Du <yd3127@mun.ca>
2019-06-24 16:35:41 -0230

    set up node and express for backend

---
+++ post-search-app/server.js
@@ -0,0 +1,10 @@
+// console.log('Hello!');
+const http = require('http');
+const app = require('./backend/app');
+
+const port = process.env.PORT || 3000;
+
+app.set('port', port);
+const server = http.createServer(app);
+
+server.listen(port);
