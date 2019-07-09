458c8fdab05dc8c81e4d3f3cf54a49a9880d5f6f
Yusong Du <yd3127@mun.ca>
2019-07-09 00:11:46 -0230

    fetch posts data at backend

--- post-search-app/backend/app.js
+++ post-search-app/backend/app.js
@@ -2,13 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('first middleware');
-    next();
-});
-
-app.use((req, res, next) => {
-    res.send('Hello from express!');
+app.use('/api/posts', (req, res, next) => {
+    // res.send('Hello from express!');
+    const posts = [
+        {
+            id: 'fadf12421l',
+            title: 'First server-side post',
+            content: 'This is coming from the server'
+        },
+        {
+            id: '123oooooo',
+            title: 'Second server-side post',
+            content: 'This is coming from the server lol'
+        }
+    ];
+    res.status(200).json({
+        message: "posts are successful to be fetched.",
+        posts: posts
+    });
 });
 
 module.exports = app;
