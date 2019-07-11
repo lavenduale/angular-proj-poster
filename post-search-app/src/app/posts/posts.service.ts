6f5b6aa03ed5414825bb5665d2e4139ce7568e3d
Yusong Du <yd3127@mun.ca>
2019-07-11 19:15:16 -0230

    added post request and console the post

--- post-search-app/src/app/posts/posts.service.ts
+++ post-search-app/src/app/posts/posts.service.ts
@@ -30,7 +30,10 @@ export class PostsService {
             title: title,
             content: content
         };
-        this.postList.push(post);
-        this.postsUpdated.next([...this.postList]);
+        this.http.post<{message: string}>('http://localhost:3000/api/posts', post).subscribe((responseData) => {
+            console.log(responseData);
+            this.postList.push(post);
+            this.postsUpdated.next([...this.postList]);
+        });
     }
 }
